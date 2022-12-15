73b856e329898d7c4b84646a866d6b286c23e9fd
kksbz <hellghost7@naver.com>
2022-12-15 22:19:23 +0900

    Study

---
+++ Study/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Study
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
