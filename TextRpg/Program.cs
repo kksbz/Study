73d406c2fcf49d8ade83bd9c54748bccf6cc31ca
kksbz <hellghost7@naver.com>
2022-12-23 17:43:56 +0900

    Text

---
+++ TextRpg/Program.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+
+namespace TextRpg
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //타이틀 씬
+            Console.WriteLine("=============================");
+            Console.WriteLine("===== 타     이      틀 =====");
+            Console.WriteLine("=============================");
+            Console.WriteLine();
+            Console.WriteLine("계속 하려면 아무거나 입력하세요.");
+            Console.ReadLine();
+
+            //선택지를 포함한 이벤트->유저가 선택 가능한 형태
+            string[] item = { "장검", "지팡이", "활", "창", "머신건" };
+            int[] itemInfo = {10, 11,15,3,200 };
+            Random startItem = new Random();
+            int a = startItem.Next(0, 5);
+            Console.WriteLine("items: {0}를 (데미지{1}) 얻었습니다", item[a], itemInfo[a]);
+
+            Console.WriteLine("=====================================================");
+            Console.WriteLine("당신은 동굴을 탐험하다 돌이 쌓인 돌탑을 발견했습니다." +
+                "\n돌탑을 살펴보자 틈사이로 빛나는 무언가를 발견했습니다.");
+            Console.WriteLine("=====================================================");
+
+            Console.WriteLine();
+            Console.WriteLine("선택1: 돌을 치워 무언가를 얻는다");
+            Console.WriteLine("선택2: 무시하고 갈 길 간다.");
+            Console.WriteLine("선택1 -> 1 입력 , 선택2 -> 2 입력");
+            int userInPut = 0;
+            for (int index = 0; index < 1; index++)
+            {
+                int.TryParse(Console.ReadLine(), out userInPut);
+                if (userInPut == 1)
+                {
+                    Console.WriteLine("1 번 선택");
+                }
+                else if (userInPut == 2)
+                {
+                    Console.WriteLine("2 번 선택");
+                }
+                else
+                {
+                    Console.WriteLine("잘못 입력 하셨습니다. 다시 선택하세요.");
+                    index--;
+                }
+            }
+            Console.WriteLine();
+            switch (userInPut)
+            {
+                case 1:
+                    Console.WriteLine("낡은 보물상자를 얻었다!");
+                    break;
+                case 2:
+                    Console.WriteLine("무시하고 탐험시작.");
+                    break;
+            }
+            Console.WriteLine();
+
+            //전투
+            int playerHp = 100;
+            int playerAttack = 17;
+            Random number = new Random();
+
+            string[] monsters = new string[] { "늑대", "오크", "슬라임", "닭" };
+            int[] monAttack = new int[4];
+            int[] monHp = new int[4];
+            for(int index = 0; index <5; index++)
+            {
+                if(index == 0)
+                {
+                    monAttack[index] = number.Next(7, 15 + 1);
+                    monHp[index] = number.Next(40, 60 + 1);
+                }
+                else if(index == 1)
+                {
+                    monAttack[index] = number.Next(10, 20 + 1);
+                    monHp[index] = number.Next(80, 110 + 1);
+                }
+                else if(index == 2)
+                {
+                    monAttack[index] = number.Next(5, 10 + 1);
+                    monHp[index] = number.Next(70, 90 + 1);
+                }
+                else if(index == 3)
+                {
+                    monAttack[index] = number.Next(15, 30 + 1);
+                    monHp[index] = number.Next(20, 30 + 1);
+                }
+            }
+            int monsterNumber = number.Next(0, 4 - 1);
+            int monsterAttack = monAttack[monsterNumber];
+            int monsterHp = monHp[monsterNumber]; 
+
+            Console.WriteLine("{0} 이(가) 나타났다! 전투준비\n체력: {1}, 공격력: {2}", monsters[monsterNumber],monsterHp ,monsterAttack);
+            Console.WriteLine();
+            
+            while (playerHp > 0)
+            {
+                monsterHp -= playerAttack;
+                Console.WriteLine("플레이어가 {0} 에게 {1} 데미지 만큰 공격!\n남은 플레이어 HP = {2}, {3} 의 HP = {4}",
+                    monsters[monsterNumber], playerAttack, playerHp, monsters[monsterNumber], monsterHp);
+                if (monsterHp <= 0)
+                {
+                    Console.WriteLine("승리! {0} 을(를) 잡았습니다.", monsters[monsterNumber]);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine();
+                    playerHp -= monsterAttack;
+                    Console.WriteLine("{0} 이(가) 플레이어에게 {1} 데미지 만큰 공격!\n남은 플레이어 HP = {2}, {3} 의 HP = {4}",
+                    monsters[monsterNumber], monsterAttack, playerHp, monsters[monsterNumber], monsterHp);
+                    if (playerHp <= 0)
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("{0} 에게 죽었습니다", monsters[monsterNumber]);
+                        break;
+                    }
+                }
+                Console.WriteLine();
+            }
+            /*
+             * 1. 사용자로부터 2개의 문자열을 읽어서 같은지 다른지 화면에 출력하는 프로그램 작성
+             * ex) 첫번째 문자열: Hello
+             *     두번째 문자열: World
+             *     2개의 문자열은 다릅니다.
+             *-Equals 메서드를 사용하지 않을 것.
+             *hint - Lenth 를 비교 ?
+             */
+            //Console.Write("문자를 입력하시오: ");
+            //string messageA = Console.ReadLine();
+            //Console.Write("문자를 입력하시오: ");
+            //string messageB = Console.ReadLine();
+            //if(messageA == messageB)
+            //{
+            //    Console.WriteLine("2개의 문자열 {0} 와 {1} 은(는) 같습니다.", messageA, messageB);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("2개의 문자열 {0} 와 {1} 은(는) 다릅니다.", messageA, messageB);
+            //}
+
+            ///*
+            // * 2. 5개의 음료(콜라, 물, 스프라이트, 주스, 커피)를 판매하는 자판기 머신을 구현해보기
+            // * 사용자가 1부터 5사이의 숫자를 입력함.
+            // * 선택한 음료를 출력함.
+            // * 1~5 이외의 숫자를 선택하면 오류 메시지 출력함.
+            // * hint - switch 문이나, if/else 문을 사용한다.
+            // */
+            //int[] drink = { 1, 2, 3, 4, 5 };
+            //int userInPut = 0;
+            //for(int index = 0; index <1; index++)
+            //{
+            //    Console.Write("콜라(1), 물(2), 스프라이트(3), 주스(4), 커피(5) 중 선택하시오.");
+            //    int.TryParse(Console.ReadLine(), out userInPut);
+            //    Console.WriteLine();
+            //    switch (userInPut)
+            //    {
+            //        case 1 :
+            //        Console.WriteLine("콜라 를 뽑았습니다.");
+            //            break;
+            //        case 2 :
+            //        Console.WriteLine("물 을 뽑았습니다.");
+            //            break;
+            //        case 3 :
+            //            Console.WriteLine("스프라이트 를 뽑았습니다.");
+            //            break;
+            //        case 4 :
+            //            Console.WriteLine("주스 를 뽑았습니다.");
+            //            break;
+            //        case 5 :
+            //            Console.WriteLine("커피 를 뽑았습니다.");
+            //            break;
+            //        default :
+            //            Console.WriteLine("잘못 입력하셨습니다.");
+            //            Console.WriteLine();
+            //            index -= 1;
+            //            break;
+            //    }
+            //}
+            //Console.WriteLine();
+            ///*
+            // * 3. 배열 days[]를 아래와 같이 초기화하고 배열 요소의 값을 다음과 같이 출력하는 프로그램 작성
+            // * 배열 days[]는 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
+            // * ex) 1월은 31일까지 입니다..
+            // *     2월은 29일까지 입니다..
+            // * hint - 배열의 초기화는 중괄호를 사용한다.
+            // */
+            //int[] days = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+            //for(int index = 0; index <12;index++)
+            //{
+            //    Console.WriteLine("{0} 월은 {1} 일까지 입니다.", index + 1, days[index]);
+            //}
+
+        }
+    }
+}
--- WhatIsArray/Program.cs
+++ WhatIsArray/Program.cs
@@ -219,16 +219,23 @@ namespace WhatIsArray //내가 정한 프로그램 이름이다.
 
             //LAB 1.크기가 100인 배열을 1부터 100 사이의 난수로 채우고 배열 요소 중에서 최대값을 찾는 프로그램 작성.
             //가독성 좋게 출력
-            //int[] sizeH = new int[100];
-            //Random randomNumber = new Random();
-            //for(int index = 0; index < 100; index++)
-            //{
-            //    sizeH[index] = randomNumber.Next(1, 100 + 1);
-            //    //Console.WriteLine("sizeH{0}의 값은 {1}입니다", index, sizeH[index]);
-            //}
-            //Console.WriteLine("sizeH배열의 최대값은 {0} 입니다.", sizeH.Max());
+            //int형 배열 sizeH의 길이를 100으로 선언
+            int[] sizeH = new int[100];
+            //sizeH 안에 0~100사이의 숫자를 랜덤하게 넣기위해 Random문 선언
+            Random randomNumber = new Random();
+            //for문 시작 loop: 100번 도는 루프
+            for (int index = 0; index < 100; index++)
+            {
+                //index = 0 일때 시작해서 99까지돔
+                //sizeH[0]번 부터 1~100까지의 랜덤한 수 하나를 뽑아 저장함
+                //100+1인 이유 (최대값은 포함X기 때문)
+                sizeH[index] = randomNumber.Next(1, 100 + 1);
+                //sizeH에 값이 제대로 들어갔는지 출력
+                Console.WriteLine("sizeH{0}의 값은 {1}입니다", index, sizeH[index]);
+            } //for문 종료
+            Console.WriteLine("sizeH배열의 최대값은 {0} 입니다.", sizeH.Max());
 
-            //LAN 2.사과를 제일 좋아하는 사람찾기
+            //LAB 2.사과를 제일 좋아하는 사람찾기
             //사람들 5명 (사람1,사람2,...)에게 아침에 먹는 사과 개수를 입력하도록 요청하는 프로그램 작성
             //데이터 입력이 마무리 되면 누가 가장 많은 사과를 아침으로 먹었는지 출력한다. (기본형)
             //-이상한 입력 예외처리
@@ -238,85 +245,142 @@ namespace WhatIsArray //내가 정한 프로그램 이름이다.
             //                              정렬 도전시 유저 입력X, 데이터는 난수로 100~1000개 정도의 값
             //                              중복 제거, 시간초는 전혀 상관 없음.
 
-            //기본+변형1
-            //int[] appleCount = new int[5];
-            //for(int index = 0; index < 5; index++)
-            //{
-            //    Console.Write("아침에 먹은 사과 개수를 입력하시오.: ");
-            //    int.TryParse(Console.ReadLine(), out appleCount[index]);
-            //    if (appleCount[index] > 0)
-            //    {
-            //    }
-            //    else
-            //    {
-            //        Console.WriteLine("잘못 입력하였습니다.");
-            //        index--;
-            //    }
-            //}
-            ////Console.WriteLine("{0},{1},{2},{3},{4}", appleCount[0], appleCount[1], appleCount[2], appleCount[3], appleCount[4]);
-            //Console.WriteLine("사람{0}가 가장 많은 {1} 개수의 사과를 먹었습니다.", Array.IndexOf(appleCount, appleCount.Max()) + 1,
-            //    appleCount.Max()); //가장 많이 먹은 사람과 개수 구하기
-            //Console.WriteLine("사람{0}가 가장 적은 {1} 개수의 사과를 먹었습니다.", Array.IndexOf(appleCount, appleCount.Min()) + 1,
-            //    appleCount.Min()); //(변형1) 가장 적개 먹은 사람과 개수 구하기
-
-            //변형2
-            Random randomCount = new Random();
-            int userInPut = 0;
-            for (int index = 0; index <= 1; index++) //입력 예외 처리for문
+            //기본 + 변형1
+            //5명의 사람이 먹은 사과의 갯수를 입력하기 위해  int형 배열 appleCount 의 크기를 5로 선언
+            int[] appleCount = new int[5];
+            //입력값 예외처리 for문 시작 배열은 0부터 시작하므로 index < 5 로 정함 loop: 5번 도는 루프
+            for (int index = 0; index < 5; index++)
             {
-                Console.Write("입력하시오: ");
-                int.TryParse(Console.ReadLine(), out userInPut); //string형식으로 입력받은걸 int형식으로 변환해서 담음
-                if (userInPut > 0) //userInPut이 정수일때 실행
+                Console.Write("아침에 먹은 사과 개수를 입력하시오.: ");
+                //string형식으로 입력받은 것을 int형식으로 변환하여 appleCount[index]안에 저장함
+                int.TryParse(Console.ReadLine(), out appleCount[index]);
+                //if문 시작 조건: 입력받은 appleCount[index]의 값이 0 보다 큰 정수일 때
+                if (appleCount[index] > 0)
                 {
-                    break; //즉시 종료
                 }
-                else //userInPut이 정수가 아닐때 실행
+                //입력받은 appleCount의 값이 정수가 아니면 실행
+                else
                 {
-                    Console.WriteLine("XX다시 입력하시오.XX ");
-                    index--; //for문을 다시 돌리기위해 index-1을함
+                    //정수가 아니므로 오류메시지 출력
+                    Console.WriteLine("잘못 입력하였습니다.");
+                    //appleCount[index] 안에 다시 값을 입력받아 저장해야하기 때문에 index-- 증감식으로 재실행하기위해 선언
+                    index--;
                 } //if문 종료
             } //for문 종료
-            int[] appleCount = new int[userInPut]; //userInPut만큼 appleCount[]에 저장
-            Console.WriteLine("appleCount의 길이는{0}", appleCount.Length); //appleCount안에 잘들어갔나 확인용
-            int[] appleEat = new int[userInPut]; //나중에 사람번호째로 정렬하기위해 appleEat에 초기값저장함
 
-            for (int index = 0; index < userInPut; index++) //for문 시작
+            //appleCount배열안에 제대로 값이 들어갔는지 확인하기 위한 for문 시작
+            for (int index = 0; index < 5; index++)
             {
-                appleCount[index] = randomCount.Next(0, 100 + 1);
-                //appleCount[0]번부터 userInPut-1수까지 랜덤(0~99)값을 저장
-                Console.WriteLine("사람{0}이 먹은 사과 개수는 {1} 입니다", index + 1, appleCount[index]);
-                appleEat[index] = index + 1; //먹은사람 1번부터~userInPut까지 구하기위해 appleEat에 저장
-            } //for문 끝
-            Console.WriteLine();//공백
+                //출력 index가 0부터 시작하므로 사람1부터 표시하기위해 index + 1 시킴
+                Console.WriteLine("사람{0} 이 먹은 사과 개수는?: {1}", index + 1, appleCount[index]);
+            } //for문 종료
 
-            for (int index = 0; index < userInPut; index++) //첫번째 for문 시작 
+            //appleCOunt배열안에 최대값과 최소값을 찾기위해 Array.IndexOf문과 .Max, .Min 문을 사용함
+            //Array.IndexOf(주소값을 찾고자하는 배열이름 , (찾고자하는 주소값의 들어있는 값)) 배열은 0번부터시작이라 사람1...5까지 출력하기위해 +1시킴
+            Console.WriteLine("사람{0}가 가장 많은 {1} 개수의 사과를 먹었습니다.", Array.IndexOf(appleCount, appleCount.Max()) + 1,
+                appleCount.Max()); //가장 많이 먹은 사람과 개수 구하기
+            Console.WriteLine("사람{0}가 가장 적은 {1} 개수의 사과를 먹었습니다.", Array.IndexOf(appleCount, appleCount.Min()) + 1,
+                appleCount.Min()); //(변형1) 가장 적개 먹은 사람과 개수 구하기
+
+            //아래문제랑 공백 구분
+            Console.WriteLine();
+
+            //변형2 -먹은 사과의 개수 순으로 정렬 ->알고리즘을 잘 모르겠다면 버블 정렬을 도전 해볼 것.
+            int userInPut = 0;
+            //입력값 예외처리하기 위한 for문 시작 사람 수만 입력하면되므로 loop: 2번돔
+            for (int index = 0; index <= 1; index++)
             {
-                for (int index2 = 0; index2 < userInPut - 1; index2++) //두번째 for문 시작 userInPut-1번(포함X)까지 반복
+                Console.Write("사람 수를 입력하시오: ");
+                //string형식으로 입력받은 값을 int형식으로 변환해서 저장
+                int.TryParse(Console.ReadLine(), out userInPut);
+                //입력값 예외처리를 위한 if 문 실행 조건: userInPut이 0보다 큰 정수일 때
+                if (userInPut > 0)
                 {
-                    if (appleCount[index2] > appleCount[index2 + 1]) //if문시작 ex) appleCount[0] > appleCount[1] 일때 시작
-                    {
-                        //appleCount[0] > appleCount[1]보다 클때 실행됨
-                        int temp = appleCount[index2 + 1]; //새로 정한 temp변수에 appleCount[1]값을 저장
-                        appleCount[index2 + 1] = appleCount[index2]; //apppleCount[1]값이 더큰 값인 appleCount[0]의 값을 저장
-                        appleCount[index2] = temp; //appCount[0]값에 temp값을 저장 (temp값에는 더 작은 appleCount[1]의 값이 있음)
+                    //0보다 큰 정수를 입력받았으므로 break로 즉시 종료
+                    break;
+                }
+                //위에 조건이 만족하지않을때 실행
+                else
+                {
+                    //입력된 값이 0보다큰 정수가 아니므로 오류메시지 출력
+                    Console.WriteLine("XX다시 입력하시오.XX ");
+                    //for문을 다시 돌리기위해 index값을 --로 감소시킴
+                    index--;
+                } //if문 종료
+            } //for문 종료
 
-                        temp = appleEat[index2 + 1]; //사람구하기 위식과 동일
+            //입력받은 userInPut의 값이 사람이 먹은 사과의 갯수가 되기 때문에 appleCount2 (위에 문제에 쓴 변수명이라 2를 붙임)
+            //라는 int형 배열을 만들어 그안에 int로 변형된 입력값을 저장
+            int[] appleCount2 = new int[userInPut];
+            //appleCount2 배열안에 입력값이 잘 들어갔는지 확인용 출력문
+            Console.WriteLine("appleCount2의 길이는{0}", appleCount.Length);
+            //가장 적게 먹은 사과갯수를 뒤에 버블정렬로 배열의 Index주소마다 값이 바뀔거기때문에
+            //마지막출력문에 사람순서를 가장적게먹은 사람순서로 출력하기위한 appleEat (int형 배열 선언) 안에 userInPut 초기값을 저장함
+            int[] appleEat = new int[userInPut];
+            //사과 먹은 갯수를 0~100사이의 난수로 저장하기위한 Random 변수 선언
+            Random randomCount = new Random();
+            //사과 먹은 갯수를 appleCount2 안에 저장하기 위한 for문 시작
+            for (int index = 0; index < userInPut; index++)
+            {
+                //appleCount2[0]번부터 userInPut (userInPut크기가 곧 배열의 길이임) 안에
+                //.Next 함수를 이용하여 0~100+1 (최대값은 포함X라 0~99까지 넣기위해 +1)의 범위안에 랜덤숫자 1개를 저장
+                appleCount2[index] = randomCount.Next(0, 100 + 1);
+                //appleCount2안에 값이 잘 저장되었는지 확인용 출력문
+                Console.WriteLine("사람{0}\t이 먹은 사과 개수는 {1}\t개 입니다", index + 1, appleCount2[index]);
+                //먹은 사람 1번부터 userInPut까지 구하기 위해 appleEat에 index + 1(index가 0부터 시작이므로 +1)
+                //값 저장 (appleEat배열안에 1,2,...userInPut+1의 정수가 저장됨)
+                appleEat[index] = index + 1;
+            } //for문 끝
+            //공백처리
+            Console.WriteLine();
+            //가장 적게 먹은 사과갯수 부터 가장 많은 갯수 순서로 구하기 위한 for문 시작 loop: userInPut - 1 만큼 루프(배열은0부터시작)
+            for (int index = 0; index < userInPut; index++)
+            {
+                //2번째 for문 시작 userInPu - 1번까지 반복 (배열은 0부터 시작하기 때문)
+                for (int index2 = 0; index2 < userInPut - 1; index2++)
+                {
+                    //appleCount2 앞에 주소값과 뒤에 주사값을 비교하기 위한 if문 시작. (조건: 앞의 주소값 > 뒤의 주소값)
+                    if (appleCount2[index2] > appleCount2[index2 + 1])
+                    {
+                        //버블정렬사용
+                        //appleCount[0] > appleCount[1]보다 클때 실행됨
+                        //temp라는 새로운 변수를 선언하고 그안에 크기가 작은 뒤에값을 저장
+                        int temp = appleCount2[index2 + 1];
+                        //앞의값과 뒤의값을 바꾸기위해 뒤에값에 앞의값을 저장
+                        appleCount2[index2 + 1] = appleCount2[index2]; //apppleCount[1]값이 더큰 값인 appleCount[0]의 값을 저장
+                        //temp안에 저장된 뒤에값을 앞에값에 저장
+                        appleCount2[index2] = temp;
+
+                        //위식과 동일 먹은 사람의 순번도 구하기위해 작성함
+                        temp = appleEat[index2 + 1];
                         appleEat[index2 + 1] = appleEat[index2];
                         appleEat[index2] = temp;
                     } //if문 끝
                 } //두번째 for문 끝
             } //첫번째 for문 끝
-
-            for (int index = 0; index < userInPut; index++) //for문 시작 userInPut만큼 반복
+            //버블정렬이 잘 작동했는지 확인하기 위한 foreach문 시작
+            //appleCount2안에있는 첫번째 요소(Element)부터 시작해서 끝까지 반복
+            foreach (int element in appleCount2)
             {
-                Console.WriteLine("{0}번째\t사람이 먹은 사과 개수는 {1} 입니다", appleEat[index], appleCount[index]);
-                //appleEat[index]값은 사람을 표시, appleCount[index]값은 먹은 사과 개수 표시
+                //눈으로 보기 위한 출력문
+                Console.WriteLine("appleCount2 안에 {0} 값이 들어있음", element);
+            } //foreach문 종료
+            //공백구분
+            Console.WriteLine();
+            //몇번째 사람이 가장 적게 먹었는지 최소값부터 보여주는 출력문을 위한 for문 시작 loop: userInPut - 1만큼 루프(배열은0부터시작) 
+            for (int index = 0; index < userInPut; index++)
+            {
+                //appleEat[] = 몇번째 사람, appleCount2[] = 먹은 사과 갯수
+                Console.WriteLine("사람{0}\t이 먹은 사과 개수는 {1}\t개 입니다", appleEat[index], appleCount2[index]);
             } //for문 끝
 
-            foreach (int index in appleCount) //버블정렬이 잘 실행됐는지 확인함.
-            {
-                Console.WriteLine("{0}", index);
-            }
+            /*
+             * 레퍼런스 게임 직접 해보면서 내가 지금까지 배운 것들로 무엇을 어디까지 구현할 수 있을지 마인드맵 그려보기.
+             * -타이틀 씬
+             * -선택지를 포함한 이벤트
+             * -얻을 수 있는 보상 or 패널티를 보여주는 이벤트 ex)골드,아이템획득 or 스탯감소 등..
+             * -전투 씬 ex)몬스터 등장 전투
+             */
 
             // 프로그램은 여기서 끝난다.
         }
