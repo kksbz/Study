ba9247258f60d17949b2e2748a92cc85c1b390e2
kksbz <hellghost7@naver.com>
2023-01-05 17:42:10 +0900

    [kks]2023-01-05

--- WhitIsParameter/Program.cs
+++ WhitIsParameter/Program.cs
@@ -6,17 +6,17 @@ namespace WhitIsParameter
         static void Main(string[] args)
         {
             Description desc = new Description();
-            //int num1 = 10;
-            //int num2 = 20;
-            ////값 형식 전달임 (함수안에서는 실행되지만 함수밖으로 나오면 안바뀜)
-            //desc.ValueTypeParam(num1, num2); 
-            //Console.WriteLine("first: {0}, second: {1}", num1, num2);
-            ////값 형식 전달임 (함수안에서는 실행되지만 함수밖으로 나오면 안바뀜)
+            int num1 = 10;
+            int num2 = 20;
+            //값 형식 전달임 (함수안에서는 실행되지만 함수밖으로 나오면 안바뀜)
+            desc.ValueTypeParam(num1, num2);
+            Console.WriteLine("first: {0}, second: {1}", num1, num2);
+            //값 형식 전달임 (함수안에서는 실행되지만 함수밖으로 나오면 안바뀜)
 
-            ////참조 형식 전달임 (원본변수를 수정할 수 있음 원본변수의 주소를 넘겨주는 방식임)
-            //desc.RefTypeParam(ref num1, ref num2);
-            //Console.WriteLine("first: {0}, second: {1}", num1, num2); //함수실행후 메인에서도 바뀌어서 나옴
-            ////참조 형식 전달임
+            //참조 형식 전달임 (원본변수를 수정할 수 있음 원본변수의 주소를 넘겨주는 방식임)
+            desc.RefTypeParam(ref num1, ref num2);
+            Console.WriteLine("first: {0}, second: {1}", num1, num2); //함수실행후 메인에서도 바뀌어서 나옴
+            //참조 형식 전달임
 
             ////반환형 형식 전달임 (아무값도 없는 변수를 함수에 대입하면 함수안에서 변수를 초기화해서 반환됨)
             //int number;
